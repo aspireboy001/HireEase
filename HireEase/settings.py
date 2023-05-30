f97f1cf2c1278b65582b5863c770beaf563f6f09
aspireboy001 <97575801+aspireboy001@users.noreply.github.com>
2023-05-30 15:31:49 +0530

    Update settings.py

--- HireEase/settings.py
+++ HireEase/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pccshireease@gmail.com'
-EMAIL_HOST_PASSWORD = 'hyfyopapmiaccrrw'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'pccshireease@gmail.com'
